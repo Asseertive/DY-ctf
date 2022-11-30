251141dd6592216bbe2236e6356fcddae9f31a60
suk-6 <admin@suk.kr>
2022-12-01 00:37:25 +0900

    simple_sqli

--- simple-sqli/init/init.sql
+++ simple-sqli/init/init.sql
@@ -1,9 +1,9 @@
-CREATE TABLE dyhs.simple_sqli(
+CREATE TABLE dyhs.dy_simple_sqli(
   id VARCHAR(100) PRIMARY KEY,
   pw VARCHAR(100)
 );
 
-INSERT into dyhs.simple_sqli VALUE (
+INSERT into dyhs.dy_simple_sqli VALUE (
     "admin",
-    "DY{fakeflag}"
+    "DY{flag}"
 );
---
+++ simple-sqli/src/chall/index.php
@@ -0,0 +1,10 @@
+<?php
+  include "../dbconn.php";
+  if(preg_match('/dy|_|\.|\(\)/i', $_GET['id'])) exit("No Hack haha");
+  if(preg_match('/dy|_|\.|\(\)/i', $_GET['pw'])) exit("No Hack haha");
+  $query = "select pw from dy_simple_sqli where id='{$_GET['id']}' and pw='{$_GET['pw']}'";
+  echo "<hr>query : <strong>{$query}</strong><hr><br>";
+  $result = @mysqli_fetch_array(mysqli_query($db,$query));
+  if($result['pw']) echo "<h2>{$result['pw']}</h2>";
+  highlight_file(__FILE__);
+?>
