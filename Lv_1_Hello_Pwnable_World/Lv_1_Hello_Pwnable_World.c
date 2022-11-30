e1830b0503ed52c1a1b6980e799dc3da3cb3e010
KyoungOh_0329 <mazoola12@gmail.com>
2022-11-30 19:36:10 +0900

    Add pwn

---
+++ Lv_1_Hello_Pwnable_World/Lv_1_Hello_Pwnable_World.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void call(){
+    printf("DY{flag}/n");
+}
+
+int main(){
+    int A;
+    double a;
+
+    for(int i=1; i<=5; i++){
+        printf("--%d트째--\n",i);
+        printf("crack_me : ");
+        scanf("%d",&A);
+        printf("crack_me : ");
+        scanf("%lf",&a);
+        if (A+a == 5){
+            call();
+            return 0;
+        }
+    }
+    printf("re_try :)");
+}
