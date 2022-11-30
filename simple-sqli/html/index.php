4b72f9e28cb879dcfb66813035b3f4983046e731
suk-6 <admin@suk.kr>
2022-11-30 12:29:28 +0900

    Update simple_sqli

--- simple-sqli/html/index.php
+++ simple-sqli/html/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<?php
+  include "./config.php";
+  $db = dbconnect();
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[id])) exit("No Hack ~_~");
+  if(preg_match('/prob|_|\.|\(\)/i', $_GET[pw])) exit("No Hack ~_~");
+  $query = "select pw from simple_sqli where id='{$_GET[id]}' and pw='{$_GET[pw]}'";
+  echo "<hr>query : <strong>{$query}</strong><hr><br>";
+  $result = @mysqli_fetch_array(mysqli_query($db,$query));
+  if($result['pw']) echo $result['pw'];
+  highlight_file(__FILE__);
+?>
