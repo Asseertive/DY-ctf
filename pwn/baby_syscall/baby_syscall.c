faf7b47a86625eff25a32561c8af7c0b9889ea9d
KyoungOh_0329 <mazoola12@gmail.com>
2022-12-01 14:28:48 +0900

    Add_baby_syscall

---
+++ pwn/baby_syscall/baby_syscall.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+void g00dg00dg00d(){
+    printf("hello~");
+    system("/bin/sh");
+}
+
+int main(){
+    char key[10];
+    printf("key ? :");
+    scanf("%s",key);
+    if(strcmp(key,"Asseertive") == 0){
+        g00dg00dg00d();
+    }
+    
+    else{
+        puts(":p");
+    }
+    return 0;
+}
