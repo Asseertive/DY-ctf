fc945699bb1d8d9300418bbb827900661f1bd122
suk-6 <admin@suk.kr>
2023-01-02 02:30:46 +0000

    Add uPnP Setting sh

---
+++ upnp/delete.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+IP=10.10.0.101
+
+upnpc -d $(IP) 9000 TCP
+upnpc -d $(IP) 9001 TCP
+upnpc -d $(IP) 9002 TCP
+upnpc -d $(IP) 9003 TCP
+upnpc -d $(IP) 9004 TCP
+upnpc -d $(IP) 9005 TCP
+upnpc -d $(IP) 9006 TCP
+upnpc -d $(IP) 9007 TCP
---
+++ upnp/setting.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+IP=10.10.0.101
+
+upnpc -a $(IP) 9000 9000 TCP
+upnpc -a $(IP) 9001 9001 TCP
+upnpc -a $(IP) 9002 9002 TCP
+upnpc -a $(IP) 9003 9003 TCP
+upnpc -a $(IP) 9004 9004 TCP
+upnpc -a $(IP) 9005 9005 TCP
+upnpc -a $(IP) 9006 9006 TCP
+upnpc -a $(IP) 9007 9007 TCP
